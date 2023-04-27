3683cfe81d7f9f0e1160a66b2c69f2131a1f3ef8
WangGrundy <2101005@uad.ac.uk>
2023-04-27 14:23:49 +0100

    stuff

--- CountWord.cpp
+++ CountWord.cpp
@@ -1,3 +1,5 @@
+#define _SILENCE_AMP_DEPRECATION_WARNINGS
+
 #include <iostream>
 #include <amp.h>
 #include <thread>
@@ -8,6 +10,11 @@
 #include <time.h>
 #include <array>
 #include <numeric>
+#include <algorithm>
+#include <condition_variable>
+#include <semaphore>
+#include <barrier>
+
 
 using namespace concurrency;
 using std::chrono::duration_cast;
@@ -17,11 +24,17 @@ typedef std::chrono::steady_clock the_serial_clock;
 typedef std::chrono::steady_clock the_amp_clock;
 
 bool timerStarted = false;
-the_amp_clock::time_point start1;
 the_amp_clock::time_point end1;
 
 std::atomic_int elementsDoneAtomic = -1;
 std::atomic_int countAtomic = 0;
+
+std::condition_variable cv;
+bool lockTimer = false;
+std::mutex mutexForTimer;
+
+std::atomic_bool atomicBool = false;
+std::barrier allThreads(5);
 
 std::string GenerateRandomString(int sizeOfWord) {
 	std::string temp;
@@ -70,64 +83,57 @@ void WordCount(std::string word, const std::vector<std::string>& vector, int& co
 	}*/
 }
 
-void WordCount_P_Atomic(std::string word, const std::vector<std::string>& vector, int threadsNum, int& count) {
+// doesn't work effectively
+//void WordCount_P_Atomic(std::string word, const std::vector<std::string>& vector, int threadsNum, int& count) { 
+//
+//	auto claim_index = []() -> int { //each thread can "claim" some spot in the vector using the value returned by fetch_add
+//		return elementsDoneAtomic.fetch_add(1, std::memory_order_seq_cst); //add 1 to element checked counter  //single total order exists in which all threads observe all modifications in the same order with {memory_order_seq_cst}
+//	};  // returns the value immediately preceding the effects of this function in the modification order of *this, AKA elementsDoneAtomic + 1
+//
+//	int claimed_index;
+//
+//	//if (!timerStarted) {
+//	//	timerStarted = true;
+//	//	start1 = the_amp_clock::now(); //start timer
+//	//	
+//	//}
+//
+//	//start1 = the_amp_clock::now(); //start timer
+//
+//	while ((claimed_index = claim_index()) < vector.size()) {
+//		std::cout << claimed_index << std::endl;
+//
+//		//if a word in the vector is equal to the word we are looking for,
+//		if (vector[claimed_index] == word) {
+//			countAtomic++; //add 1 to the word count
+//		}
+//	}
+//
+//	end1 = the_amp_clock::now(); //end timer
+//	count = countAtomic; //set the count with the atomic count
+//}
 
-	auto claim_index = []() -> int { //each thread can "claim" some spot in the vector using the value returned by fetch_add
-		return elementsDoneAtomic.fetch_add(1, std::memory_order_seq_cst); //add 1 to element checked counter  //single total order exists in which all threads observe all modifications in the same order with {memory_order_seq_cst}
-	};  // returns the value immediately preceding the effects of this function in the modification order of *this, AKA elementsDoneAtomic + 1
+void WordCount_P_Atomic_2(std::string word, const std::vector<std::string>& vector, int threadTotal, int& count, int threadID) {
 
-	int claimed_index;
+	int threadNumber = threadID + 1;
+	int threadCountTemp = threadNumber;
 
-	//if (!timerStarted) {
-	//	timerStarted = true;
-	//	start1 = the_amp_clock::now(); //start timer
-	//	
+	//std::unique_lock<std::mutex> uniquelock1(mutexForTimer);
+
+	//if (lockTimer == false) {
+	//	std::cout << "waiting" << std::endl;
+	//	cv.wait(uniquelock1);
+	//	//std::cout << "GO" << std::endl;
 	//}
+	
+	allThreads.arrive_and_wait();
 
-	//start1 = the_amp_clock::now(); //start timer
-
-	while ((claimed_index = claim_index()) < vector.size()) {
-		std::cout << claimed_index << std::endl;
-
-		//if a word in the vector is equal to the word we are looking for,
-		if (vector[claimed_index] == word) {
-			countAtomic++; //add 1 to the word count
+	if (threadNumber == 1) { //account for the 0th element
+		if (vector[0] == word) {
+			countAtomic++;
 		}
 	}
 
-	end1 = the_amp_clock::now(); //end timer
-	count = countAtomic; //set the count with the atomic count
-}
-
-void concurrent_method_timer()
-{
-	// WHEN ONE SIGNAL START COMES IN:
-		// START TIMING
-
-	// WHEN ALL SIGNAL STOPS COME IN:
-		// END TIMER.
-
-	// RETURN TIME
-
-}
-
-void WordCount_P_Atomic_2(std::string word, const std::vector<std::string>& vector, int threadTotal, int& count, int thread) {
-
-
-
-	// SIGNAL START
-
-
-	int threadNumber = thread + 1; //doesn't account for the 0th element; //rework this
-	int threadCountTemp = threadNumber;
-
-	//if (!timerStarted) { 
-	//	timerStarted = true;
-	//	start1 = the_amp_clock::now(); //start timer
-
-	//}
-	start1 = the_amp_clock::now(); //start timer
-	
 	while (threadCountTemp < vector.size()) {
 
 		if (vector[threadCountTemp] == word) {
@@ -137,9 +143,7 @@ void WordCount_P_Atomic_2(std::string word, const std::vector<std::string>& vect
 		threadCountTemp += threadTotal;
 	}
 
-	// SIGNAL END
-
-	//end1 = the_amp_clock::now(); //end timer
+	end1 = the_amp_clock::now(); //end timer
 	count = countAtomic; //set the count with the atomic count
 }
 
@@ -153,18 +157,30 @@ long long TestNonParallel(int& count, std::string word, int sizeOfVector, const 
 
 long long TestParallel(int& count, std::string word, int sizeOfVector, std::vector<std::string>& vector, int threadsNum) {
 	
+	//test
+	atomicBool = false;
+
 	std::mutex count_mutex;
 	std::mutex elementsDone_mutex;
 	int elementsDone = 0;
 	std::vector<std::thread> threadVector;
+	lockTimer = false;
 
-	for (int i = 0; i < threadsNum; i++) {
-		threadVector.push_back(std::thread(&WordCount_P_Atomic_2, word, vector, threadsNum, std::ref(count), i));
+	{
+		std::lock_guard<std::mutex> lg(mutexForTimer); //lock guard
+		for (int i = 0; i < threadsNum; i++) { //do work
+			threadVector.push_back(std::thread(&WordCount_P_Atomic_2, word, vector, threadsNum, std::ref(count), i));
+		}
+		lockTimer = true; //set bool to true
+		cv.notify_all(); //notify all threads that thread creation has been complete
 	}
-
+	//start timing
+	the_amp_clock::time_point start1 = the_amp_clock::now(); //start timer
+	
 	for (int i = 0; i < threadsNum; i++) {
 		threadVector[i].join();
 	}
+
 	return duration_cast<nanoseconds>(end1 - start1).count(); ///////////////////////////////////////
 }
 
@@ -177,41 +193,33 @@ int main() {
 	int cores = std::thread::hardware_concurrency();
 	int threadsNum = cores - 1;
 	std::cout << "number of cores: " << cores << std::endl;
-	int sizeOfVector = 100;
+	int sizeOfVector = 100000;
 	int sizeOfRandomWords = 5;
 	std::vector<std::string> vector;
 
 	GenerateVectorOfWords(word, vector, sizeOfVector, sizeOfRandomWords);
-	for (std::string word : vector) {
+	/*for (std::string word : vector) {
 		std::cout << word << std::endl;
+	}*/
+
+	int testRepetitions = 10;
+	std::vector<long long> parallelTestTimes,  nonParallelTestTimes;
+
+	for (int i = 0; i < testRepetitions; i++) {
+		//std::cout << "starting test[ " << i << " ] " << std::endl;
+		nonParallelTestTimes.push_back(TestNonParallel(count, word, sizeOfVector, vector));
+		parallelTestTimes.push_back(TestParallel(count2, word, sizeOfVector, vector, threadsNum));
 	}
 
-	long long time_taken = TestNonParallel(count, word, sizeOfVector, vector);
-	long long time_taken2 = TestParallel(count2, word, sizeOfVector, vector, threadsNum);
+	std::sort(nonParallelTestTimes.begin(), nonParallelTestTimes.end());
+	std::sort(parallelTestTimes.begin(), parallelTestTimes.end());
 
-	std::cout << "[1]SERIAL: COUNT IS : " << count << " AND TIME TAKEN: " << std::setprecision(7) << time_taken << "ms" << std::endl;
-	std::cout << "[1]PARALLEL: COUNT IS : " << count2 << " AND TIME TAKEN: "<< std::setprecision(7) << time_taken2 << "ms" << std::endl;
+	int medianPlacement = (testRepetitions / 2) - 1;
 
-	std::cout << "starting second test" << std::endl;
+	//for (long long num : parallelTestTimes) {
+	//	std::cout << num << std::endl;
+	//}
 
-	timerStarted = false;
-	elementsDoneAtomic = -1;
-	std::atomic_int countAtomic = 0;
-
-	/*time_taken = TestNonParallel(count, word, sizeOfVector, vector);
-	time_taken2 = TestParallel(count2, word, sizeOfVector, vector, threadsNum);
-
-	std::cout << "[2]SERIAL: COUNT IS : " << count << " AND TIME TAKEN: " << std::setprecision(7) << time_taken << "ms" << std::endl;
-	std::cout << "[2]PARALLEL: COUNT IS : " << count2 << " AND TIME TAKEN: " << std::setprecision(7) << time_taken2 << "ms" << std::endl;*/
-
-
-
-
-
-	/*std::string test;
-	char abc[] = "ABC";
-	test.reserve(2);
-	test += abc[0]; test += abc[2];
-	std::cout << test;*/
-
+	std::cout << "SERIAL: COUNT IS : " << count << " AND TIME TAKEN: "  << nonParallelTestTimes[medianPlacement] << "ms" << std::endl;
+	std::cout << "PARALLEL: COUNT IS : " << count2 << " AND TIME TAKEN: "<< parallelTestTimes[medianPlacement] << "ms" << std::endl;
 }
